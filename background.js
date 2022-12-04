576722faddb1965e7cb3ebed83d183da659b32f8
TrePe <peter@synchtank.net>
2022-12-04 07:21:56 +0100

    Update to manifest 3. Fix logging when the day did not start.

--- background.js
+++ background.js
@@ -1,14 +1,15 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete' && tab.url) {
-        var matches = tab.url.match(/\.com\/([0-9]{4})\/day\/([0-9]+)/);
+        var matches = tab.url.match(/\.com\/([0-9]{4})\/day\/([0-9]+)$/);
         var timestamp = Math.floor(Date.now() / 1000);
         if (matches !== null) {
             var year = matches[1];
             var day = matches[2];
             chrome.tabs.sendMessage(tab.id, {text: 'get_user_name'}, function(response) {
+                if (response.error) return;
                 var userName = response.userName;
                 var part = response.part;
-                chrome.storage.sync.get(['times'], function(result) {
+                chrome.storage.sync.get(['aoc_times'], function(result) {
                     if (result.times === undefined) {
                         result = {times: {}};
                     }
@@ -17,12 +18,10 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
                     if (result.times[year][day][part] === undefined) {
                         if (userName) {
                             var url = 'https://rb5ncgzaxj.execute-api.eu-central-1.amazonaws.com/Prod/'+year+'/day/'+day+'?part='+part+'&name='+encodeURIComponent(userName);
-                            var xhr = new XMLHttpRequest();
-                            xhr.open("GET", url);
-                            xhr.send();
+                            fetch(url).then((data) => { console.log(url); });
                         }
                         result.times[year][day][part] = timestamp;
-                        chrome.storage.sync.set({times: result.times}, function() {
+                        chrome.storage.sync.set({aoc_times: result.times}, function() {
                             console.log('User: '+userName);
                             console.log(result.times);
                         });
--- content.js
+++ content.js
@@ -1,12 +1,14 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if (msg.text === 'get_user_name') {
         var el = document.querySelector('header .user');
-        if (el === null) sendResponse({userName:'', part:1});
+        var error = (document.getElementsByClassName('day-desc').length == 0);
+        if (el === null) sendResponse({userName:'', part:1, error: error});
         else {
             var i = el.innerHTML.indexOf('<');
             sendResponse({
                 userName: (i <= 0 ? el.innerHTML : el.innerHTML.substr(0, i - 1)),
-                part: (document.getElementById('part2') !== null) + 1
+                part: (document.getElementById('part2') !== null) + 1,
+                error: error
             });
         }
     }
