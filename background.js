4fb1178c2dac188e0d8b545425356ef9cc71ab9f
Matus Horvath <matus.horvath@gmail.com>
2023-03-04 16:18:58 +0100

    Fix async/await with manifest V2 and Firefox

--- background.js
+++ background.js
@@ -1,15 +1,18 @@
 const aocBotUrl = 'https://7b79gj2si4.execute-api.eu-central-1.amazonaws.com/Prod/start';
 
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+// With manifest V2 and Firefox, chrome.* APIs don't return promises, but browser.* APIs do.
+const browserOrChrome = typeof browser !== "undefined" ? browser : chrome;
+
+browserOrChrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     'use strict';
     if (changeInfo.status == 'complete' && tab.url) {
         const matches = tab.url.match(/\.com\/([0-9]{4})\/day\/([0-9]+)/);
         const timestamp = Math.floor(Date.now() / 1000);
         if (matches !== null) {
             const [, year, day] = matches;
-            const { userName, part, error } = await chrome.tabs.sendMessage(tab.id, { text: 'get_user_name' });
+            const { userName, part, error } = await browserOrChrome.tabs.sendMessage(tab.id, { text: 'get_user_name' });
             if (error) return;
-            let { aoc_times: times } = await chrome.storage.sync.get(['aoc_times']);
+            let { aoc_times: times } = await browserOrChrome.storage.sync.get(['aoc_times']);
             if (times === undefined) times = {};
             if (times[year] === undefined) times[year] = {};
             if (times[year][day] === undefined) times[year][day] = {};
@@ -33,7 +36,7 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
                 }
 
                 times[year][day][part] = timestamp;
-                await chrome.storage.sync.set({ aoc_times: times });
+                await browserOrChrome.storage.sync.set({ aoc_times: times });
                 console.log('User:', userName);
                 console.log(times);
             }
